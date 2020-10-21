ac88d3b8fca42def40153a67691b36d8e4a82522
RodolfoChavarria <rodolfochavarria@metricamovil.com>
2020-10-21 12:16:57 -0600

    editrol07

--- src/controllers/querycontrol.js
+++ src/controllers/querycontrol.js
@@ -82,7 +82,7 @@ qryCtrl.EditRol = async (req, res) => {
         let id_rol = req.body.id_Rol_Edit;//agregado Edit
         let new_rol = req.body.name_Rol_Edit;
         console.log(req.body);
-        let text = 'SELECT updateRol($1,$2) ORDER BY nombrerol ASC';
+        let text = 'SELECT updateRol($1,$2)';
         let values = [id_rol,new_rol];
         await pool.query(text, values);
         res.json({ status: 'Rol Edited' });
