cb92ee9d0428379823c900dc9ff5cb0a0812de0a
RodolfoChavarria <rodolfochavarria@metricamovil.com>
2020-11-24 07:34:15 -0800

    monitoreditcp01

--- src/controllers/querymonitor.js
+++ src/controllers/querymonitor.js
@@ -9,10 +9,14 @@ qryCtrlMonitor.EditCheckPoint = async (req, res) => {
         console.log(req.body);
          var checkpoints=req.body;
          let text=('SELECT * FROM ruta_checkpoint WHERE id_ruta=$1');
-         let values=[checkpoints.id_ruta];
+         let values=[checkpoints.id_route];
          const {rows} = await pool.query(text,values);
          console.log(rows.length);
          console.log(rows[0]);
+            // fecha=ruta.fechaIni.anio+"-"+ ruta.fechaIni.mes+"-"+ruta.fechaIni.dia;
+            // fllegada=ruta.fechaFin.anio+"-"+ ruta.fechaFin.mes+"-"+ruta.fechaFin.dia;
+            // hsalida=ruta.horaIni.hora+":"+ruta.horaIni.minutos+":"+"00";
+            // hllegada=ruta.horaFin.hora+":"+ruta.horaFin.minutos+":"+"00";
          if(rows.length==0){
              res.json({status:'Not Found!'});
          }
@@ -20,7 +24,7 @@ qryCtrlMonitor.EditCheckPoint = async (req, res) => {
             for (let y = 0; y < rows.length; y++) {
                 let text2 = 'SELECT edicionRuta_Checkpoint($1,$2,$3,$4,$5)';
                 //pedir hora y fecha ya que lo solicita la db para ingresar manualmente
-                let values2 = [checkpoints.id_ruta, checkpoints.checkpoints[y].id_punto, checkpoints.checkpoints[y].name_punto,checkpoints.checkpoints[y].hora,checkpoints.checkpoints[y].fecha];
+                let values2 = [checkpoints.id_route, checkpoints.checkpoints[y].id_punto, checkpoints.checkpoints[y].name_punto,checkpoints.checkpoints[y].hora,checkpoints.checkpoints[y].fecha];
                 await pool.query(text2, values2);
             }
             res.json({ status: 'ok' });
