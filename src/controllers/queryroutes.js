4fb3b40394cd71bd1a694882aa2327b24d0694d8
RodolfoChavarria <rodolfochavarria@metricamovil.com>
2020-11-24 05:22:41 -0800

    getrutas06

--- src/controllers/queryroutes.js
+++ src/controllers/queryroutes.js
@@ -394,11 +394,14 @@ qryCtrlRoutes.QueryAll = async (req, res) => {
         console.log(routes.rows[0]);
         console.log(cPoints.rows[0]);
         console.log(cPoints.rows[0].id_ruta);
+        console.log(cPoints.rows.length);
+        console.log(routes.rows.length);
         var rutaCompleta = [];
         for (var i = 0; i < cPoints.rows.length; i++) {
             for(var j=0; j< routes.rows.lenght;j++){
                 if (cPoints.rows[i].id_ruta == routes.rows[j].id_ruta) {
                     rutaCompleta.push({ ruta: routes.rows[j], checkpoits: cPoints.rows[i] });
+                    console.log(j);
                 }
             }
                 
