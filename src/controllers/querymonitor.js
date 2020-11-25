7d4fcd9bdef4f4e1acea9593918842bbc268fe04
RodolfoChavarria <rodolfochavarria@metricamovil.com>
2020-11-25 00:41:27 -0800

    Monitor00

--- src/controllers/querymonitor.js
+++ src/controllers/querymonitor.js
@@ -13,14 +13,19 @@ qryCtrlMonitor.EditCheckPoint = async (req, res) => {
          const {rows} = await pool.query(text,values);
          console.log(rows.length);
          console.log(rows[0]);
+         let text2 = "DELETE FROM Ruta_Checkpoint WHERE id_ruta=$1";
+        let values2=[checkpoints.id_routep];
+        await pool.query(text2,values2);
+        console.log('borro');
          if(rows.length==0){
              res.json({status:'Not Found!'});
          }
          else{
             for (let y = 0; y < rows.length; y++) {
-                let text2 = 'SELECT edicionRuta_Checkpoint($1,$2,$3,$4,$5)';
+                //let text2 = 'SELECT edicionRuta_Checkpoint($1,$2,$3,$4,$5)';
+                let text2 = 'SELECT createRuta_Checkpoint($1,$2,$3,$4,$5)';
                 //pedir hora y fecha ya que lo solicita la db para ingresar manualmente funcionando ver formatos fecha y hora al pedir
-                let values2 = [checkpoints.id_route, checkpoints.checkpoints[y].id_punto, checkpoints.checkpoints[y].name_punto,checkpoints.checkpoints[y].fecha,checkpoints.checkpoints[y].hora];
+                let values2 = [checkpoints.id_routep, checkpoints.checkpoints[y].id_punto, checkpoints.checkpoints[y].name_punto,checkpoints.checkpoints[y].fecha,checkpoints.checkpoints[y].hora];
                 await pool.query(text2, values2);
             }
             res.json({ status: 'ok' });
@@ -29,8 +34,6 @@ qryCtrlMonitor.EditCheckPoint = async (req, res) => {
         catch(e){
             console.log('ERROR EDITANDO CP Monitor',e);
         }
-         
-
 };
 
 
