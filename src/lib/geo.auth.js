e8fc09a6ef18535589f4e3219806fa51c2b8c836
RodolfoChavarria <rodolfochavarria@metricamovil.com>
2020-11-30 11:28:02 -0800

    GeoTabLogin07

--- src/lib/geo.auth.js
+++ src/lib/geo.auth.js
@@ -48,17 +48,39 @@ geoCtrl.loginGeo = async (req, res) => {
                 console.log(ide_user);
                 let session = await api.authenticate();
                 let text = 'SELECT createUsuario_Geotab($1,$2,$3)';
-                let values = [ide_user/*session.credentials.sessionId*/, session.credentials.userName, session.credentials.database];//username es email
+                let values = [ide_user, session.credentials.userName, session.credentials.database];//username es email
                 await pool.query(text, values);
                 ///setpath recibe session.credentials.sessionId para darle el path correcto
                 let text2 = 'SELECT setPath($1,$2)';
                 let values2 =[ide_user/*session.credentials.sessionId*/, req.body.path];
                 await pool.query(text2,values2);
-                /////////////////////////////////////////////////
-                console.log(session.credentials.sessionId);
+
                 let text3 = 'SELECT setSessionId($1,$2)';
                 let values3 =[ide_user, session.credentials.sessionId];
                 await pool.query(text3,values3);
+                /////////////////////agregado 28 nov 11am para iniciar con grupos
+                const group = await api.call("Get", {
+                    typeName: "User",
+                    search: {
+                        name: req.body.email
+                    },
+                });
+                console.log(group.length);
+                var groups = [];
+                console.log(group[0]);
+                for (var i = 0; i < group[0].companyGroups.length; i++) {
+                    groups.push(group[0].companyGroups[i].id);
+                }
+                console.log(groups[0].companyGroups[0].id);
+                console.log(groups.length);
+
+                for (i = 0; i < groups.length; i++) {
+                    let text3 = ('SELECT setGrupo($1,$2');
+                    let values2 = [User.email, groups[0].companyGroups[i].id];
+                    await pool.query(text3, values2);
+                }
+                /////////////////////////////////////////////////
+                console.log(session.credentials.sessionId);
                 res.json({ email: session.credentials.userName });
                 //await res.redirect('http://35.206.82.124/finalizar_registro');
                 console.log('insertado usuario GEOTAB en DB');    
@@ -80,8 +102,41 @@ geoCtrl.loginGeo = async (req, res) => {
                 let values2=[ide_usuario,req.body.database];
                 await pool.query(text2,values2);
                 console.log('debio guardar');
+                /////AGREGADO 11AM PONER GRUPOS
+                let del=('SELECT deleteGrupos($1)');
+                let val=[ide_usuario];
+                await pool.query(del,val);
+                // let text3 = 'SELECT * FROM vistaObtenerUsuario WHERE correo = $1';
+                // let values3 = [req.body.email];
+                // let result = await pool.query(text3, values3);
+                const group = await api.call("Get", {
+                    typeName: "User",
+                    search: {
+                        name: req.body.email
+                    },
+                });
+                console.log(group.length);
+                var groupss = [];
+                console.log(group[0]);
+                for (var j = 0; j < group[0].companyGroups.length; j++) {
+                    groupss.push(group[0].companyGroups[j].id);
+                }
+                console.log(groupss[0].companyGroups[0].id);
+                console.log(groupss.length);
+
+                for (var l = 0; l < groupss.length; l++) {
+                    let text3 = ('SELECT setGrupo($1,$2');
+                    let values2 = [User.email, groupss[0].companyGroups[l].id];
+                    await pool.query(text3, values2);
+                }
+
+                let textt = 'SELECT * FROM vistaObtenerUsuario WHERE correo = $1';
+                let valuess = [req.body.email];
+                let results = await pool.query(textt, valuess);
+
+                res.json({ status: results.rows[0] });
                 //////////////////////////////////se agrego despues de rows
-                res.json({status: rows[0],db:req.body.database,path:req.body.path,sessionId:session.credentials.sessionId });//cambio a status para llegue igual que login SESSION DATABASE
+                //res.json({status: rows[0],db:req.body.database,path:req.body.path,sessionId:session.credentials.sessionId });//cambio a status para llegue igual que login SESSION DATABASE
             }
         }, (error) => {
             res.json('Wrong Credentials!');
@@ -104,52 +159,11 @@ geoCtrl.registerGeo =async (req,res)=>{
         let text = 'SELECT setGeotab($1,$2,$3,$4)';
         let values = [User.email, User.username, User.lastname, User.telephone];
         await pool.query(text, values);
-        //setGrupo(ide_usuario varchar(60),ide_grupo varchar(60))
-
-
-
-    let text2 = 'SELECT * FROM vistaObtenerUsuario WHERE correo = $1';
-    let values2 = [req.body.email];
-    let { rows } = await pool.query(text2, values2);
-    console.log(rows[0]);
-    console.log(rows[0].sessionid);
-    console.log(rows[0].path);
-    console.log(rows[0].bd);
-    console.log(rows[0].correo);
-    const api = await new GeotabApi({ credentials: { userName: rows[0].correo, database: rows[0].bd, sessionId: rows[0].sessionid }, path: rows[0].path });
-    console.log(api);
-    // console.log(user + db + session + servo);
-    //     const api = await new GeotabApi({ credentials: { userName: user, database: db, sessionId: session }, path: servo });
-    //     console.log(api);
-    
-        const group = await api.call("Get", {
-            typeName: "User",//si es user y es el companyGroups
-            search: {
-                name: rows[0].correo
-            },
-        });
-        var groups=[];
-        console.log(group[0]);
-        for(var i=0;i<group[0].companyGroups.length;i++){
-            groups.push(group[0].companyGroups[i].id);
-        }
-        console.log(groups[0].companyGroups[0].id);
-        console.log(groups.length);
-
-        for(i=0;i<groups.length;i++){
-        let text3 = ('SELECT setGrupo($1,$2');
-        let values2 = [User.email,groups[0].companyGroups[i].id];
-        await pool.query(text3,values2);
-        }
+       
         let textt = 'SELECT * FROM vistaObtenerUsuario WHERE correo = $1';
         let valuess = [req.body.email];
         let result = await pool.query(textt, valuess);
-
-        // let text2 = 'SELECT * FROM vistaObtenerUsuario WHERE correo =$1';
-        // let value =[User.email];
-        // const {rows} = await pool.query(text2,value);
-        console.log(result.rows);
-        res.json({status:result.rows});//status:'Registered!',id_rol:rows.id_rol});
+        res.json({status:result.rows[0]});//status:'Registered!',id_rol:rows.id_rol});
     } catch (e) {
         console.log(e);
     }
@@ -157,3 +171,44 @@ geoCtrl.registerGeo =async (req,res)=>{
 
 
 module.exports = geoCtrl;
+
+
+
+ //setGrupo(ide_usuario varchar(60),ide_grupo varchar(60))
+
+
+
+   
+    //const api = await new GeotabApi({ credentials: { userName: rows[0].correo, database: rows[0].bd, sessionId: rows[0].sessionid }, path: rows[0].path });
+    // const api = await new GeotabApi({credentials:{userName:rows[0].correo,database:rows[0].bd,sessionId:rows[0].sessionid},path:rows[0].path});
+    // console.log(api);
+   
+    
+    //     const group = await api.call("Get", {
+    //         typeName: "User",//si es user y es el companyGroups
+    //         search: {
+    //             name: rows[0].correo
+    //         },
+    //     });
+        
+    //     var groups=[];
+    //     console.log(group[0]);
+    //     for(var i=0;i<group[0].companyGroups.length;i++){
+    //         groups.push(group[0].companyGroups[i].id);
+    //     }
+    //     console.log(groups[0].companyGroups[0].id);
+    //     console.log(groups.length);
+
+    //     for(i=0;i<groups.length;i++){
+    //     let text3 = ('SELECT setGrupo($1,$2');
+    //     let values2 = [User.email,groups[0].companyGroups[i].id];
+    //     await pool.query(text3,values2);
+    //     }
+    //     let textt = 'SELECT * FROM vistaObtenerUsuario WHERE correo = $1';
+    //     let valuess = [req.body.email];
+    //     let result = await pool.query(textt, valuess);
+
+    //     // let text2 = 'SELECT * FROM vistaObtenerUsuario WHERE correo =$1';
+    //     // let value =[User.email];
+        //     // const {rows} = await pool.query(text2,value);
+        //     console.log(result.rows);
