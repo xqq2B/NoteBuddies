93f84123fa06ed9f6fcfc0311ba489330cac71da
RodolfoChavarria <rodolfochavarria@metricamovil.com>
2020-12-28 11:12:13 -0700

    finaltest002

--- src/controllers/querymonitor.js
+++ src/controllers/querymonitor.js
@@ -297,7 +297,7 @@ qryCtrlMonitor.QueryExceptions = async (req, res) => {
             const result= await api.call('GetFeed', {
                 typeName: 'ExceptionEvent', /*fromVersion: token, */search: {
                     //deviceSearch: { id: rows[j].id_vehiculo }, QUITADO PARA VER RESULTADOS
-                    ruleSearch: { id: idRuleEntrandoStart[0].id }, /*fromDate: req.body.testeofecha/*'2020-01-01T00:01:00'*/festimadasalirDB//rows[j].horainicioestimada//fechaInicioRuta//poner fecha de cuando va a arrancar
+                    ruleSearch: { id: idRuleEntrandoStart[0].id }, fromDate:/* req.body.testeofecha/*'2020-01-01T00:01:00'*/festimadasalirDB//rows[j].horainicioestimada//fechaInicioRuta//poner fecha de cuando va a arrancar
                 }, 
             });
                // .then(result => {
